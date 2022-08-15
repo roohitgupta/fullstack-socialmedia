ea5efc655419e9e97761612f9306d6615c5e8cb6
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-08-15 22:33:14 +0530

    build database connection & setup bodyParser for futher image uplode.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
-import express from "express";
-import mongoose from "mongoose";
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const app = express();
+const app = express(); 
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+mongoose.connect("mongodb://localhost:27017/exoketo", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=> app.listen(PORT, ()=> console.log(`Server running on port: ${PORT}`)))
+    .catch((error)=> console.log(error.message));
 
