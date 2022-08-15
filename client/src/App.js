ad6ceec9224c5caca7831e1d65d7a7bb2be967a7
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-08-15 21:28:05 +0530

    project initialized

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      this is important;;
+    </div>
+  )
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import mongoose from "mongoose";
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const app = express();
+
