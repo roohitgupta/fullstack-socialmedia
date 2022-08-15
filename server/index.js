470e0302dfa2a9cd46c1fa0856bf3fc9250bca26
roohitgupta <96112427+roohitgupta@users.noreply.github.com>
2022-08-15 22:49:21 +0530

    succefull connected express application.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+// const express = require('express');
+// const cors = require('cors');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+import express from 'express'
+import cors from 'cors'
+import bodyParser from 'body-parser'
+import mongoose from 'mongoose'
+
+import postRoutes from './routes/posts.js';
 
 const app = express(); 
+
+app.use('/posts', postRoutes)
+
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
